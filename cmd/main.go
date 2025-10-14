175c5f73774f5b344a03645469b8fbb60bb76e28
FatalError <sms7182@gmail.com>
2025-10-14 07:38:04 +0330

    fix for traccar

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 
@@ -40,23 +39,56 @@ type Envelope struct {
 	Payload map[string]interface{} `json:"payload"`
 }
 
+type Coords struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Accuracy  float64 `json:"accuracy"`
+	Speed     float64 `json:"speed"`
+	Heading   float64 `json:"heading"`
+	Altitude  float64 `json:"altitude"`
+}
+
+type LocationCoords struct {
+	Timestamp string `json:"timestamp"`
+	Coords    Coords `json:"coords"`
+}
+
+type Payload struct {
+	Location LocationCoords `json:"location"`
+	DeviceID string         `json:"device_id"`
+}
+
 func producerHandler(writer *kafka.Writer) func(http.ResponseWriter, *http.Request) {
 	return http.HandlerFunc(func(wr http.ResponseWriter, req *http.Request) {
-		body, err := io.ReadAll(req.Body)
+
+		var data Payload
+		err := json.NewDecoder(req.Body).Decode(&data)
 		if err != nil {
-			http.Error(wr, "Failed to read request body", http.StatusInternalServerError)
-			log.Println("Read error:", err)
+			http.Error(wr, "Invalid JSON", http.StatusBadRequest)
 			return
 		}
 
-		var test map[string]interface{}
-		if err := json.Unmarshal(body, &test); err != nil {
-			http.Error(wr, "Invalid JSON format", http.StatusBadRequest)
-			log.Println("JSON validation error:", err)
-			return
-		}
+		var test = make(map[string]interface{})
+		// if err := json.Unmarshal(body, &test); err != nil {
+		// 	http.Error(wr, "Invalid JSON format", http.StatusBadRequest)
+		// 	log.Println("JSON validation error:", err)
+		// 	return
+		// }
+		test["id"] = data.Location.Timestamp
+		test["deviceId"] = data.DeviceID
+		test["heading"] = data.Location.Coords.Heading
+		test["speed"] = data.Location.Coords.Speed
+		test["accuracy"] = data.Location.Coords.Accuracy
+		test["altitude"] = data.Location.Coords.Altitude
+		test["latitude"] = data.Location.Coords.Latitude
+		test["longitude"] = data.Location.Coords.Longitude
 		fields := []SchemaField{
-			{Type: "int32", Field: "id"},
+			{Type: "string", Field: "id"},
+			{Type: "string", Field: "deviceId"},
+			{Type: "float", Field: "heading"},
+			{Type: "float", Field: "speed"},
+			{Type: "float", Field: "accuracy"},
+			{Type: "float", Field: "altitude"},
 			{Type: "float", Field: "latitude"},
 			{Type: "float", Field: "longitude"},
 		}
@@ -70,7 +102,7 @@ func producerHandler(writer *kafka.Writer) func(http.ResponseWriter, *http.Reque
 			Value: valueBytes,
 		}
 
-		log.Println("Message sent to Kafka:", string(body))
+		log.Println("Message sent to Kafka:", (data))
 
 		if err := writer.WriteMessages(req.Context(), msg); err != nil {
 			log.Println("Kafka write error:", err)
@@ -84,7 +116,7 @@ func main() {
 	fmt.Print("hi")
 	_ = godotenv.Load("../.env")
 	kafkaURL := "localhost:9094"
-	kafkaTopic := "my_location111"
+	kafkaTopic := "test1983"
 	fmt.Println("kafkaurl:", kafkaURL)
 	kafkaWriter := getKafkaWriter(kafkaURL, kafkaTopic)
 	defer kafkaWriter.Close()
